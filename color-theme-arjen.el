24fd0b97e666cf42ee590a3cd2a582774391a7ac
Arjen Wiersma <arjen@wiersma.org>
2008-04-27 14:04:56 +0200

    updated comments to state that it is not part of GNU Emacs

--- color-theme-arjen.el
+++ color-theme-arjen.el
@@ -5,6 +5,8 @@
 ;; Author: Arjen Wiersma <arjen@wiersma.org>
 ;; Keywords: local
 
+;; This file is NOT part of GNU Emacs
+
 ;; This file is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
 ;; the Free Software Foundation; either version 3, or (at your option)
